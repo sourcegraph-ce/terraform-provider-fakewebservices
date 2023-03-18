1ff96e4f7840c845b8eb0e2ded0cb10b57fabdab
Sourcegraph <batch-changes@sourcegraph.com>
2023-03-18 00:31:51 +0000

    Replacing log package with logrus

--- fws/resource_fws_database.go
+++ fws/resource_fws_database.go
@@ -2,7 +2,7 @@ package fws
 
 import (
 	"fmt"
-	"log"
+	log "github.com/sourcegraph-ce/logrus"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-fakewebservices/client"
